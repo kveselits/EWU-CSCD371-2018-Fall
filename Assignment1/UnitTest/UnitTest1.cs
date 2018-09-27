db0ff9b3592520798c063b64f13828575000ccdb
Kristofer Veselits <kveselits@eagles.ewu.edu>
2018-09-27 14:24:20 -0700

    Second commit.

--- Assignment1/UnitTest/UnitTest1.cs
+++ Assignment1/UnitTest/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace Assignment1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void UnitTestName()
         {
             string myValue = "Kris Veselits";
             string expectedOutput = $@">>Hello, what is your name?
