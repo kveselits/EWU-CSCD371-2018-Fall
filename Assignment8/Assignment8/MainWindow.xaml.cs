3375b2e4a3fea07879d80288aed6d9455a11178c
Kris <kveselits@eagles.ewu.edu>
2018-11-15 23:52:51 -0800

    Not nearly done

---
+++ Assignment8/Assignment8/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            /*ClickMeButton. Click += ClickMeButton_OnClick;
+            ClickMeButton.Click -= ClickMeButton_OnClick;*/
+            var button = new Button();
+            button.Content = new TextBlock {Text = "kevin"};
+            StackPanel.Children.Add(button);
+
+        }
+
+        private void ClickMeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ClickMeButton_OnClickXaml(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
