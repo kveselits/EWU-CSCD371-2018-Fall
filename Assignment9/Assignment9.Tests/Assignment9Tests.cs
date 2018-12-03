34d123ce8d0862bf9b082e45a02077bcfe08bb25
Kris <kveselits@eagles.ewu.edu>
2018-12-03 06:26:30 -0800

    Implimented InventorLastNames

--- Assignment9/Assignment9.Tests/Assignment9Tests.cs
+++ Assignment9/Assignment9.Tests/Assignment9Tests.cs
@@ -9,12 +9,36 @@ namespace Assignment9.Tests
     public class Assignment9Tests
     {
         [TestMethod]
-        public void InventorNames_Returns_List_Specified_Country()
+        public void InventorNames_Returns_List_Specified_Country_Success()
         {
             List<String> inventorNames = PatentDataAnalyzer.InventorNames("UK");
             string expectedOutput = "George Stephenson";
             Assert.AreEqual(inventorNames.ElementAt(0), expectedOutput);
 
+        }
+        [TestMethod]
+        public void InventorNames_Returns_List_Specified_Country_Failure()
+        {
+            List<String> inventorNames = PatentDataAnalyzer.InventorNames("USA");
+            string expectedOutput = "George Stephenson";
+            Assert.AreNotEqual(inventorNames.ElementAt(0), expectedOutput);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void InventorNames_Null_Exception()
+        {
+            List<String> inventorNames = PatentDataAnalyzer.InventorNames(null);
+
+        }
+
+        [TestMethod]
+        public void InventorLastNames_Returns_Different_Sequence()
+        {
+            List<String> inventorLastNames = PatentDataAnalyzer.InventorLastNames();
+            List<String> inventorNames = PatentDataAnalyzer.InventorNames();
+            bool equals = inventorLastNames.SequenceEqual(inventorNames);
+            Assert.IsFalse(equals);
         }
     }
 }
--- Assignment9/Assignment9/PatentDataAnalyzer.cs
+++ Assignment9/Assignment9/PatentDataAnalyzer.cs
@@ -6,12 +6,29 @@ namespace Assignment9
 {
     public class PatentDataAnalyzer
     {
+        public static List<string> InventorNames()
+        {
+            return PatentData.Inventors.Select(inventor => inventor.Name).ToList();
+        }
         public static List<string> InventorNames(string country)
         {
+            if (country == null) throw new ArgumentNullException(nameof(country));
             return PatentData.Inventors
                 .Where(inventor => inventor.Country.Equals(country))
                 .Select(inventor => inventor.Name)
                 .ToList();
         }
+
+        public static List<string> InventorLastNames()
+        {
+            return PatentData.Inventors
+                .OrderByDescending(Id)
+                .Select(LastName)
+                .ToList();
+        }
+
+        private static long Id(Inventor inventor) => inventor.Id;
+
+        private static string LastName(Inventor inventor) => inventor.Name.Split().Last();
     }
 }
