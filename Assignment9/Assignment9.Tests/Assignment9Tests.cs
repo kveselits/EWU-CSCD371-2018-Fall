5574890e06d9801e2699cf7dc47ebd3e46ee23a6
Kris <kveselits@eagles.ewu.edu>
2018-12-03 05:04:06 -0800

    Implimented InventorNames

--- Assignment9/Assignment9.Tests/Assignment9Tests.cs
+++ Assignment9/Assignment9.Tests/Assignment9Tests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Assignment9.Tests
@@ -7,8 +9,12 @@ namespace Assignment9.Tests
     public class Assignment9Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InventorNames_Returns_List_Specified_Country()
         {
+            List<String> inventorNames = PatentDataAnalyzer.InventorNames("UK");
+            string expectedOutput = "George Stephenson";
+            Assert.AreEqual(inventorNames.ElementAt(0), expectedOutput);
+
         }
     }
 }
--- Assignment9/Assignment9/PatentDataAnalyzer.cs
+++ Assignment9/Assignment9/PatentDataAnalyzer.cs
@@ -1,7 +1,17 @@
-﻿namespace Assignment9
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assignment9
 {
     public class PatentDataAnalyzer
     {
-        
+        public static List<string> InventorNames(string country)
+        {
+            return PatentData.Inventors
+                .Where(inventor => inventor.Country.Equals(country))
+                .Select(inventor => inventor.Name)
+                .ToList();
+        }
     }
 }
